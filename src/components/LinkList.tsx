d46e9ecb148c96638f0339e2021cfe5907763288
ikjnv <abdurauf.isokjonov@gmail.com>
2022-01-05 17:37:03 +0900

    linklist component
    * render links using linklist component

---
+++ src/components/LinkList.tsx
@@ -0,0 +1,30 @@
+import { useQuery, gql } from '@apollo/client';
+import Link   from "./Link";
+
+const FEED_QUERY = gql`
+  {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+export default function LinkList() {
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+      {data && (
+        <>
+          {data.feed.links.map((link: any) => (
+          <Link key={link.id} link={link} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
